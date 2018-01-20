7d700d47715c31f18dedf59e6ceb49009c3ef2a1
Ja <ja@lawn-128-61-62-114.lawn.gatech.edu>
2018-01-20 13:12:53 -0500

    rough draft

---
+++ assets/javascript/script.js
@@ -0,0 +1,37 @@
+// submit train name, Destination, train time, frewuency
+// store this data in Firebase
+
+//
+
+//
+
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fire base api
+
+<script src="https://www.gstatic.com/firebasejs/4.9.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBUjWM_S72JR_i95XRVW730mjVfbZBil9A",
+    authDomain: "train-time-5f07b.firebaseapp.com",
+    databaseURL: "https://train-time-5f07b.firebaseio.com",
+    projectId: "train-time-5f07b",
+    storageBucket: "",
+    messagingSenderId: "934629427852"
+  };
+  firebase.initializeApp(config);
+</script>
+
